35e0192a937300e79e050d1654963832b5abaa0b
Eray Yuztyurk <77536463+eray-yuztyurk@users.noreply.github.com>
2025-12-04 22:40:03 +0100

    Delete greetings.py

--- greetings.py
+++
@@ -1,8 +0,0 @@
-import gradio as gr
-
-def salute(input_name):
-    return "What's up, " + input_name + "?"
-
-trial = gr.Interface(fn=salute, inputs="text", outputs="text")
-
-trial.launch(server_name="0.0.0.0", server_port=7860)
